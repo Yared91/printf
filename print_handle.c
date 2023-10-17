46533c4e5a92b3372c99e3e03c46c3a09cd7c7dd
Yared91 <yaredkebede010@gmail.com>
2023-10-17 01:22:24 -0700

    print_handle.c

--- print_handle.c
+++ print_handle.c
@@ -9,7 +9,7 @@
  * @width: Width input
  * @precision: Specifies precision
  * @size: Specifies size input
- * Return: Print handled arguments
+ * Return: 1 or 2;
  */
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	int flags, int width, int precision, int size)
